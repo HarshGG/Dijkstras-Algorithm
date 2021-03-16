096b61db09f3573a8890bd35510ce5fe70677498
HarshGG <ritzred2012@gmail.com>
2021-03-15 19:58:51 -0500

    node class works

--- diskstras.py
+++ diskstras.py
@@ -5,6 +5,14 @@ display = pygame.display.set_mode((1000,600))
 pygame.display.set_caption("DIJKSTRA'S ALGORITHM")
 
 run = True
+nodes = []
+
+class Node:
+    def __init__(self,nx,ny):
+        self.x = nx
+        self.y = ny
+
+    pass
 
 while run:
     #handle events
@@ -12,13 +20,18 @@ while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run = False
+        elif event.type==pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            nodes.append(Node(pos[0],pos[1])) 
+
 
     #clear display
     display.fill(0)
 
      # draw scene
     pygame.draw.rect(display, ('#306678'), pygame.Rect(0,0,1000,600))
-    pygame.draw.circle(display, ('#fcb614'), (50, 50), 20) #(r, g, b) is color, (x, y) is center, R is radius and w is the thickness of the circle border.
+    for node in nodes:
+        pygame.draw.circle(display, ('#fcb614'), (node.x, node.y), 20) #(r, g, b) is color, (x, y) is center, R is radius and w is the thickness of the circle border.
 
     # update display
     pygame.display.flip()
