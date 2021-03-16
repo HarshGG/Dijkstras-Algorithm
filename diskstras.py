521346e4b4876841053c8edd8601d4bb57e05dfd
HarshGG <ritzred2012@gmail.com>
2021-03-15 19:35:22 -0500

    initialized display

--- diskstras.py
+++ diskstras.py
@@ -1,5 +1,26 @@
-from tkinter import *
-from tkinter import messagebox
+import pygame
+pygame.init()
 
-root = Tk()
-root.title('TIC TAC TOE!!!')
+display = pygame.display.set_mode((1000,600))
+pygame.display.set_caption("DIJKSTRA'S ALGORITHM")
+
+run = True
+
+while run:
+    #handle events
+    pygame.time.delay(10)
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run = False
+
+    #clear display
+    display.fill(0)
+
+     # draw scene
+    pygame.draw.rect(display, ('#306678'), pygame.Rect(0,0,1000,600))
+    pygame.draw.circle(display, ('#fcb614'), (50, 50), 20) #(r, g, b) is color, (x, y) is center, R is radius and w is the thickness of the circle border.
+
+    # update display
+    pygame.display.flip()
+
+
